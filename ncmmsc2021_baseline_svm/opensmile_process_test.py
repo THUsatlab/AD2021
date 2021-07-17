3edcc00f2a57909710733962167ab9782316de5b
gy-luck <62633795+gy-luck@users.noreply.github.com>
2021-07-17 19:27:21 +0800

    Add files via upload

---
+++ ncmmsc2021_baseline_svm/opensmile_process_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#该脚本和testdata测试集文件夹位于同一级目录下
+#./opensmile_test为opensmile提取的测试集特征存储路径
+#内含测试集的特征
+#提取后的特征存储在 opensmile_test文件夹下
+#后续使用fea_process脚本进行规范化
+
+import pandas as pd
+import os
+filepath=r'./opensmile_test'
+fea=r'test.csv'
+
+fn=os.path.join(filepath,fea)
+df=pd.read_csv(fn,header=None,names=['name']+[str(i) for i in range(1,1584)])
+del df['1583']
+
+df['name']=df['name'].apply(lambda x: x.strip("'"))
+os.remove(fn)
+df.to_csv(fn,index=False)
+
